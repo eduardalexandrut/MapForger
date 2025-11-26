66eb880465360f62a4d81071b8f24cc5c8ef3ab7
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2025-11-26 11:47:17 +0100

    Create and add mapforge-gameplay module

---
+++ mapforge-gameplay/src/main/java/com/example/mapforgegameplay/MapforgeGameplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapforgegameplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapforgeGameplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapforgeGameplayApplication.class, args);
+    }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "MapForger"
 
 include("mapforge-eureka")
 include("mapforge-gateway")
+include("mapforge-gameplay")
