974f739e7bc1f291a8cc852c1c1ee39b2b5361fb
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2025-11-26 19:28:31 +0100

    Create Dockerfile for mapforge-gameplay. Add postgres dependency

--- mapforge-gameplay/build.gradle.kts
+++ mapforge-gameplay/build.gradle.kts
@@ -13,5 +13,8 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    //Postgres
+    runtimeOnly("org.postgresql:postgresql")
 }
 
