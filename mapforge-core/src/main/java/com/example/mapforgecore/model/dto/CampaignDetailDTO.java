78ad763c088a1d43b4f1adb9622daf8ee6153539
eduardalexandrut <aeduardtoni@gmail.com>
2026-01-19 18:28:39 +0100

    Restructure CampaignDetailDTO structure in order to get a "flat" data structure.

--- mapforge-core/src/main/java/com/example/mapforgecore/model/dto/CampaignDetailDTO.java
+++ mapforge-core/src/main/java/com/example/mapforgecore/model/dto/CampaignDetailDTO.java
@@ -4,11 +4,16 @@ import com.example.mapforgecore.model.entity.*;
 import com.example.mapforgecore.model.entity.Campaign;
 import com.example.mapforgecore.model.entity.Map;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 public record CampaignDetailDTO(UUID id, String name, String description, Map map,
+                                UserSummaryDTO master,
+                                List<CharacterSummaryDTO> characters,
+                                List<UserSummaryDTO> players,
                                 Set<CampaignMemberDetailDTO> members) {
     public static CampaignDetailDTO fromEntity(Campaign campaign) {
         return new CampaignDetailDTO(
@@ -16,6 +21,14 @@ public record CampaignDetailDTO(UUID id, String name, String description, Map ma
                 campaign.getName(),
                 campaign.getDescription(),
                 campaign.getMap(),
+                campaign.getCampaignMembers().stream().filter(m -> Objects.equals(m.getRole(), "Master"))
+                        .map(m -> m.getCampaignActors().stream().findFirst().get().getCharacter().getCreator())
+                        .map(UserSummaryDTO::fromEntity).findAny().orElse(null),
+                campaign.getCampaignMembers().stream().flatMap(campaignMember -> campaignMember.getCampaignActors().stream())
+                        .map(CampaignActor::getCharacter).map(CharacterSummaryDTO::fromEnity).toList(),
+                campaign.getCampaignMembers().stream()
+                        .map(m -> m.getCampaignActors().stream().findFirst().get().getCharacter().getCreator())
+                        .map(UserSummaryDTO::fromEntity).toList(),
                 campaign.getCampaignMembers().stream().map(CampaignMemberDetailDTO::fromEntity).collect(Collectors.toSet())
         );
     }
