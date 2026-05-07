5af2f8a41ba2b584a1053cd751f6940262f6742d
eduardalexandrut <aeduardtoni@gmail.com>
2026-05-07 22:04:11 +0200

    feat: Resolve endpoint path error in MapController

--- mapforge-core/src/main/java/com/example/mapforgecore/controller/MapController.java
+++ mapforge-core/src/main/java/com/example/mapforgecore/controller/MapController.java
@@ -34,7 +34,7 @@ public class MapController {
         return mapService.getMapById(id);
     }
 
-    @GetMapping("/{campaignId}/map")
+    @GetMapping("/{campaignId}/campaigns")
     public Map getMapByCampaignId(@PathVariable UUID campaignId) {
         return mapService.getMapByCampaignId(campaignId);
     }
